33b387ff5785ee2213c145db39994ef1c32adc2a
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2024-02-03 22:49:43 +0000

    update

--- front-end/MapController.js
+++ front-end/MapController.js
@@ -1,47 +1,56 @@
 const markers = new Map();
 var soldiers = [];
 
-var iter = 0;
-
 async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const centerPos = {
+    lat: 51.5072,
+    lng: -0.1276,
+  };
 
-    const centerPos = {
-        lat: 28.50231,
-        lng: 77.40548
-    };
+  var map = new Map(document.getElementById("map"), {
+    zoom: 12,
+    center: centerPos,
+    mapId: "4504f8b37365c3d0",
+  });
 
-    var map = new Map(
-        document.getElementById("map"), {
-        zoom: 12,
-        center: centerPos,
-        mapId: "4504f8b37365c3d0"
+  async function getData() {
+    const url =
+      "https://5200-2a0c-5bc0-40-2e31-f8b2-a379-f82f-e798.ngrok-free.app/api/data";
+    const response = await fetch(url, {
+      mode: "cors",
+      headers: {
+        "ngrok-skip-browser-warning": "true",
+      },
     });
 
-    const interval = setInterval(function() {
-        //TODO: set soldiers to data
+    const data = await response.json();
 
-        soldiers.push({
-            pId: 1,
-            position: {
-                lat: (28.51231 + 0.0001 * iter),
-                lng: 77.40548
-            }
-        })
+    return data;
+  }
 
-        soldiers.forEach((soldier) => {
-            if(markers.has(soldier.pId)) {
-                markers.get(soldier.pId).position = new google.maps.LatLng(soldier.position.lat, soldier.position.lng);
-            }
-            else {
-                markers.set(soldier.pId, new AdvancedMarkerElement({
-                    position: soldier.pos,
-                    map
-                }));
-            }
-        });
-        iter++;
-    }, 10);
+  const interval = setInterval(async function () {
+    //TODO: set soldiers to data
+
+    soldiers = await getData();
+
+    soldiers.people.forEach((soldier) => {
+      if (markers.has(soldier.pid)) {
+        markers.get(soldier.pid).position = new google.maps.LatLng(
+          soldier.position.lat,
+          soldier.position.lng
+        );
+      } else {
+        markers.set(
+          soldier.pid,
+          new AdvancedMarkerElement({
+            position: soldier.pos,
+            map,
+          })
+        );
+      }
+    });
+  }, 10);
 }
