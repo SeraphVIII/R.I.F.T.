9a5c1f6e426cb8cde5814d95a9e3af58ff0447c4
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2024-02-03 21:03:06 +0000

    Define renderHealthInfo method

--- front-end/ScreenController.js
+++ front-end/ScreenController.js
@@ -2,17 +2,17 @@ export default class ScreenController {
   renderHealthInfo(people) {
     const healthContainer = document.querySelector(".health-container");
 
-    people.forEach();
-
-    const newSoldierHealthInterview = document.createElement(div);
-    newSoldierHealthInterview.innerHTML = `<div class="soldier-health-overview">
-    <div class="person-name"></div>
-    <p>Heartrate: <span class="person-heartrate">160</span><img class="icon" src="./assets/heart-filled.svg"></img></p>
-    <p>
-      Status: <span class="person-status critical" >critical</span>
-    </p>
-    <p>Analysis: <span class="person-analysis">Head Injury</span></p>
-
-  </div>`;
+    people.forEach((people) => {
+      const newSoldierHealthInterview = document.createElement(div);
+      newSoldierHealthInterview.innerHTML = `<div class="soldier-health-overview">
+        <div class="person-name">${people.name}</div>
+        <p>Heartrate: <span class="person-heartrate">${people.heartrate}</span><img class="icon" src="./assets/heart-filled.svg"></img></p>
+        <p>
+          Status: <span class="person-status good" >good</span>
+        </p>
+        <p>Analysis: <span class="person-analysis">none</span></p>
+    
+      </div>`;
+    });
   }
 }
