81465ae087272d11aab1d9bcc9eaa921c21625a2
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2024-02-03 21:03:29 +0000

    Update getData() method

--- front-end/FetchController.js
+++ front-end/FetchController.js
@@ -1,8 +1,14 @@
 export default class FetchController {
   async getData() {
     const url =
-      "https://7309-2a0c-5bc0-40-2e31-f8b2-a379-f82f-e798.ngrok-free.app/api/data";
-    const response = await fetch(url, { mode: "cors" });
+      "https://5200-2a0c-5bc0-40-2e31-f8b2-a379-f82f-e798.ngrok-free.app/api/data";
+    const response = await fetch(url, {
+      mode: "cors",
+      headers: {
+        "ngrok-skip-browser-warning": "true",
+      },
+    });
+
     const data = await response.json();
 
     return data;
