23bdbe8418df3488854c817a71f19ce1efac03b5
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2024-02-04 05:27:09 +0000

    Modify getData() for generated data

--- front-end/FetchController.js
+++ front-end/FetchController.js
@@ -1,16 +1,37 @@
+import { generateDataOne } from "./PathController.js";
+
 export default class FetchController {
-  async getData() {
-    const url =
-      "https://4053-2a0c-5bc0-40-2e31-f8b2-a379-f82f-e798.ngrok-free.app/api/data";
-    const response = await fetch(url, {
-      mode: "cors",
-      headers: {
-        "ngrok-skip-browser-warning": "true",
-      },
-    });
+  async getData(generatedData = false) {
+    if (!generatedData) {
+      const url =
+        "https://4053-2a0c-5bc0-40-2e31-f8b2-a379-f82f-e798.ngrok-free.app/api/data";
+      const response = await fetch(url, {
+        mode: "cors",
+        headers: {
+          "ngrok-skip-browser-warning": "true",
+        },
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    return data;
+      return data;
+    } else {
+      return new Promise((resolve, reject) => {
+        const nextDataPoint = getNextDataPoint();
+        console.log(nextDataPoint);
+        resolve(nextDataPoint);
+      });
+    }
   }
+}
+
+const dataOne = generateDataOne();
+console.log("dataONe");
+console.log(dataOne);
+
+function getNextDataPoint() {
+  if (dataOne.length == 1) return dataOne[0];
+  const data = dataOne.shift();
+  console.log(dataOne.length);
+  return data;
 }
