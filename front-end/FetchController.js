8c4fd60ee6d379d562a675984fca308b42d20743
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2024-02-04 03:43:44 +0000

    Change url

--- front-end/FetchController.js
+++ front-end/FetchController.js
@@ -1,7 +1,7 @@
 export default class FetchController {
   async getData() {
     const url =
-      "https://2a61-2a0c-5bc0-40-2e31-f8b2-a379-f82f-e798.ngrok-free.app/api/data";
+      "https://4053-2a0c-5bc0-40-2e31-f8b2-a379-f82f-e798.ngrok-free.app/api/data";
     const response = await fetch(url, {
       mode: "cors",
       headers: {
